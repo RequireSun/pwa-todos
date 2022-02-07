0d077cab1f6e3b8970a168c7611bc9a26625d38f
RequireSun <kelvinsen@outlook.com>
2022-02-07 20:17:14 +0800

    feat: do things ok, prepare for recovery

--- src/offline/api.ts
+++ src/offline/api.ts
@@ -6,42 +6,55 @@ import { API_DOMAIN, API_PREFIX, URL_ALL } from '../api/';
 // TODO sign 的返回值也放 header 里吧, 统一一点
 const REGEX_HANDLE_ALL = new RegExp(`^${API_DOMAIN}/${API_PREFIX}/`);
 const REGEX_HANDLE_ONE = new RegExp(`^${API_DOMAIN}/${API_PREFIX}/([^/]+)$`);
+const REGEX_HANDLE_DONES = new RegExp(`^${API_DOMAIN}/${API_PREFIX}/([^/]+)/(?:un)?done$`);
 const CACHE_KEY_LIST = 'pwa_todos_cache_list';
 const CACHE_KEY_SIGN = 'pwa_todos_cache_sign';
 
-function getCacheList(): DataTodo[] {
-    const list = localStorage.getItem(CACHE_KEY_LIST);
+function openCache() {
+    return caches.open('pwa_todos_cache');
+}
 
-    if (list) {
-        try {
-            return JSON.parse(list);
-        } catch (ex) {
-            return [];
-        }
+async function getCacheList(): Promise<DataTodo[]> {
+    const cache = await openCache();
+    const resp = await cache.match(CACHE_KEY_LIST);
+
+    if (resp) {
+        return resp.json();
     } else {
         return [];
     }
 }
 
-function setCacheList(list: DataTodo[]) {
-    localStorage.setItem(CACHE_KEY_LIST, JSON.stringify(list));
+async function setCacheList(list: DataTodo[]) {
+    const cache = await openCache();
+    await cache.put(CACHE_KEY_LIST, genResponse(JSON.stringify(list)));
 }
 
-function getCacheSign() {
-    const sign = localStorage.getItem(CACHE_KEY_SIGN);
-    return sign ? sign : null;
-}
-
-function setCacheSign(sign: string | null) {
-    if (sign) {
-        localStorage.setItem(CACHE_KEY_SIGN, sign);
+async function getCacheSign() {
+    const cache = await openCache();
+    const resp = await cache.match(CACHE_KEY_SIGN);
+    if (resp) {
+        return resp.text();
     } else {
-        localStorage.removeItem(CACHE_KEY_SIGN);
+        return null;
+    }
+}
+
+async function setCacheSign(sign: string | null) {
+    const cache = await openCache();
+    if (sign) {
+        await cache.put(CACHE_KEY_SIGN, genResponseText(sign));
+    } else {
+        await cache.delete(CACHE_KEY_SIGN);
     }
 }
 
 function genResponse(body: BodyInit | null) {
     return new Response(body, { headers: { 'Content-Type': 'application/json', 'pwa-offline': '1' }, status: 200 });
+}
+
+function genResponseText(body: BodyInit | null) {
+    return new Response(body, { headers: { 'pwa-offline': '1' }, status: 200 });
 }
 
 export interface HandlerConfig {
@@ -59,13 +72,17 @@ const HANDLERS: HandlerConfig[] = [
         },
         cache: async (resp: Response) => {
             const json = await resp.json();
-            setCacheList(json.list);
-            setCacheSign(json.sign);
+            await Promise.all([
+                setCacheList(json.list),
+                setCacheSign(json.sign),
+            ]);
         },
         offline: async (req: Request) => {
             const body: any = {};
-            body.list = getCacheList();
-            body.sign = getCacheSign();
+            ([body.list, body.sign] = await Promise.all([
+                getCacheList(),
+                getCacheSign(),
+            ]))
             return genResponse(JSON.stringify(body));
         },
     },
@@ -76,16 +93,16 @@ const HANDLERS: HandlerConfig[] = [
         },
         cache: async (resp: Response) => {
             const json = await resp.json();
-            setCacheSign(json.sign);
+            await setCacheSign(json.sign);
         },
         offline: async (req: Request) => {
             const { title } = await req.json();
             const sign = req.headers.get('Coordination-Sign');
-            const list = getCacheList();
+            const list = await getCacheList();
             const timeStr = (new Date()).toISOString();
             const line: DataTodo = { _id: shortId.generate(), title, done: false, createdAt: timeStr, updatedAt: timeStr };
             list.unshift(line);
-            setCacheList(list);
+            await setCacheList(list);
             return genResponse(JSON.stringify({ result: line, sign }));
         },
     },
@@ -96,18 +113,18 @@ const HANDLERS: HandlerConfig[] = [
         },
         cache: async (resp: Response) => {
             const json = await resp.json();
-            setCacheSign(json.sign);
+            await setCacheSign(json.sign);
         },
         offline: async (req: Request) => {
             const { title } = await req.json();
             const id = req.url.match(REGEX_HANDLE_ONE)![1];
             const sign = req.headers.get('Coordination-Sign');
-            const list = getCacheList();
+            const list = await getCacheList();
             const line = list.find(i => i._id === id);
             if (line) {
                 line.title = title;
             }
-            setCacheList(list);
+            await setCacheList(list);
             return genResponse(JSON.stringify({ ok: true, sign }));
         },
     },
@@ -118,13 +135,13 @@ const HANDLERS: HandlerConfig[] = [
         },
         cache: async (resp: Response) => {
             const json = await resp.json();
-            setCacheSign(json.sign);
+            await setCacheSign(json.sign);
         },
         offline: async (req: Request) => {
             const id = req.url.match(REGEX_HANDLE_ONE)![1];
             const sign = req.headers.get('Coordination-Sign');
-            const list = getCacheList();
-            setCacheList(list.filter(i => i._id !== id));
+            const list = await getCacheList();
+            await setCacheList(list.filter(i => i._id !== id));
             return genResponse(JSON.stringify({ ok: true, sign }));
         },
     },
@@ -135,17 +152,17 @@ const HANDLERS: HandlerConfig[] = [
         },
         cache: async (resp: Response) => {
             const json = await resp.json();
-            setCacheSign(json.sign);
+            await setCacheSign(json.sign);
         },
         offline: async (req: Request) => {
-            const id = req.url.match(REGEX_HANDLE_ONE)![1];
+            const id = req.url.match(REGEX_HANDLE_DONES)![1];
             const sign = req.headers.get('Coordination-Sign');
-            const list = getCacheList();
+            const list = await getCacheList();
             const line = list.find(i => i._id === id);
             if (line) {
                 line.done = true;
             }
-            setCacheList(list);
+            await setCacheList(list);
             return genResponse(JSON.stringify({ ok: true, sign }));
         },
     },
@@ -156,17 +173,17 @@ const HANDLERS: HandlerConfig[] = [
         },
         cache: async (resp: Response) => {
             const json = await resp.json();
-            setCacheSign(json.sign);
+            await setCacheSign(json.sign);
         },
         offline: async (req: Request) => {
-            const id = req.url.match(REGEX_HANDLE_ONE)![1];
+            const id = req.url.match(REGEX_HANDLE_DONES)![1];
             const sign = req.headers.get('Coordination-Sign');
-            const list = getCacheList();
+            const list = await getCacheList();
             const line = list.find(i => i._id === id);
             if (line) {
                 line.done = false;
             }
-            setCacheList(list);
+            await setCacheList(list);
             return genResponse(JSON.stringify({ ok: true, sign }));
         },
     },
@@ -187,8 +204,8 @@ const handlerAPI = async ({ request }: RouteHandlerCallbackOptions) => {
             cache(resp.clone());
         }
     } catch (ex) {
-        console.error(ex);
         resp = (await offline(request)) as Response;
+        console.warn('fetch failed, ex:', ex, ', use cache:', resp);
     }
     return Promise.resolve(resp);
 };
